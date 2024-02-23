b04d961125d1cfe000ed612ee056af482da34722
Ahsan Naqvi <syedahsan13@hotmail.com>
2024-02-23 17:55:05 +0500

    Time: 62 ms (78.72%) | Memory: 52.4 MB (24.96%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.js
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.js
@@ -10,10 +10,7 @@ var leftRightDifference = function(nums) {
     leftSum.push(0)
     let k = 0
     for(let i =0, j = nums.length-1;i < nums.length, j > 0 ; i++, j--) {
-        if(rightSum.length === nums.length) {
-            break
-        }
-        if(leftSum.length === nums.length) {
+        if(rightSum.length === nums.length || leftSum.length === nums.length ) {
             break
         }
         leftSum.push(leftSum[k] + nums[j])
